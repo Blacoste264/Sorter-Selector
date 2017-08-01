3cd80a519f4788abf9e590c1f518f60e268e5b44
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-08-01 13:57:41 -0700

    Bubble and Insertion imgs and text complete

--- algorithms.js
+++ algorithms.js
@@ -1,47 +1,6 @@
 // Global array of user inputs
 var arr = []
 
-document.getElementById('bubbleBtn').onclick = function handleClickEvent(ev) {
-    arr = [];
-    createArr(ev);
-    bubbleSort(arr);
-    document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-}
-
-document.getElementById('insertionBtn').onclick = function handleClickEvent(ev) {
-    arr = [];
-    console.log(arr)
-    createArr(ev);
-    insertionSort(arr);
-    document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-}
-
-// document.getElementById('selectionBtn').onclick = function handleClickEvent(ev) {
-//     createArr(ev);
-//     bubbleSort(arr);
-//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-// }
-
-// document.getElementById('mergerBtn').onclick = function handleClickEvent(ev) {
-//     createArr(ev);
-//     bubbleSort(arr);
-//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-// }
-
-// document.getElementById('quickBtn').onclick = function handleClickEvent(ev) {
-//     createArr(ev);
-//     bubbleSort(arr);
-//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-// }
-
-// document.getElementById('heapBtn').onclick = function handleClickEvent(ev) {
-//     createArr(ev);
-//     bubbleSort(arr);
-//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-// }
-
-
-
 function createArr() {
     // Retrieve user input values from DOM
     var i1Val = document.getElementById('i1').value;
@@ -57,7 +16,6 @@ function createArr() {
 // First sorting algorithm Bubblesort
 function bubbleSort(arr) {
     var len = arr.length;
-    console.log(len);
     for (var i = len - 1; i >= 0; i--) {
         for (var j = 1; j <= i; j++) {
             if (arr[j - 1] > arr[j]) {
@@ -88,12 +46,60 @@ function insertionSort(arr) {
     return arr;
 }
 
-// var select = document.getElementById("planetSelect");
+document.getElementById('bubbleBtn').onclick = function handleClickEvent(ev) {
+    arr = [];
+    createArr(ev);
+    bubbleSort(arr);
+    document.getElementById('newArr').innerHTML = ('<h2> Here is your Bubble sorted sorted array! ' + arr + '</h2>');
+    document.getElementById('img').innerHTML="<img src='images/Bubble-sort.gif' />";
+    document.getElementById('output').innerHTML = ('<b>How Bubble sort works...</b></br> You compare the first item with the second.' 
+    + ' If the first item is bigger than the second item. you swap them so that the bigger one stays in the second position.' 
+    + ' Then compare second with third item. If second item is bigger than the third, we swap them. otherwise, they stayed in their position.' 
+    + ' Hence, the biggest among first three is in the third position. We keep doing it until we hit the last element of the array.' 
+    + ' In that way we bubble up the biggest item of the array to the right most position of the array. Look at the inner loop in the code below.' 
+    + ' We repeat this process, starting from the last item of the array. look at the outer loop in the code below.' 
+    + ' We do this way, so that after finishing the first inner loop, the biggest one will be in the last item of the array.' 
+    + ' Then we move backward inside the outer loop. Same thing is going on...');
 
-// for (var i = 0; i < planets.length; i++) {
-//   var algorithmIndex = planets[i][0];
-//   var el = document.createElement("option");
-//   el.value = planets[i][1];
-//   el.textContent = algorithmIndex;
-//   select.appendChild(el);
+}
+
+document.getElementById('insertionBtn').onclick = function handleClickEvent(ev) {
+    arr = [];
+    createArr(ev);
+    insertionSort(arr);
+    document.getElementById('newArr').innerHTML = ('<h2> Here is your Insertion sorted array! ' + arr + '</h2>');
+    document.getElementById('img').innerHTML="<img src='images/Insertion-sort-example.gif' />";
+    document.getElementById('output').innerHTML = ('<b>How Insertion sort works... </b></br> Imagine you are playing cards. Somebody is giving you cards one by one.' 
+    + ' When you are receiving card, you are planning to put them in a way so that the smaller one is on the left. This means you want to insert them in a sorted way.' 
+    + ' If the first card you are getting is 5. Just hold the card in your hand. you don’t have to do anything. If the second card is 2, you want to put it before 5 so that the two cards you have are sorted.' 
+    + ' When you are putting the card with number 2 at the left, you are changing the position of the card 5 from first position to second position.' 
+    + ' And then first position becomes available and you put 2 there. If the third card is 4. you will start from second position. In the second position, you have card 5 which is bigger than 4.' 
+    + ' Hence you will move 5 to the third position. The next card to the left is 2 which is smaller than 4. Hence, you wont move 2. And you will insert card 4 in the second position. Then you got 10. It is bigger than the previous card which is 5.' 
+    + ' Hence, you just add it at the last position. The next card is 7. You just move the position of the card 10 to the right and insert card 7. If the last card is 3. You will have to move 10 to the right as it is bigger than 3. and then you check with the next card to the left it is 7 which is bigger than 3. you move it to the right.' 
+    + ' Similarly, you move 5, 4 to the right. And put the number 3 before 2 as 2 is smaller than 3. Congrats. You are done!');
+
+}
+
+// document.getElementById('selectionBtn').onclick = function handleClickEvent(ev) {
+//     createArr(ev);
+//     bubbleSort(arr);
+//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
+// }
+
+// document.getElementById('mergerBtn').onclick = function handleClickEvent(ev) {
+//     createArr(ev);
+//     bubbleSort(arr);
+//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
+// }
+
+// document.getElementById('quickBtn').onclick = function handleClickEvent(ev) {
+//     createArr(ev);
+//     bubbleSort(arr);
+//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
+// }
+
+// document.getElementById('heapBtn').onclick = function handleClickEvent(ev) {
+//     createArr(ev);
+//     bubbleSort(arr);
+//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
 // }
