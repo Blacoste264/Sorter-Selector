7e3a0e1a04348f0bb54535ca6f27dea5a4d6e895
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-08-02 15:50:30 -0700

    Merge sort function added

--- algorithms.js
+++ algorithms.js
@@ -13,7 +13,7 @@ function createArr() {
     arr.push(i1Val, i2Val, i3Val, i4Val, i5Val, i6Val);
 }
 
-// First sorting algorithm Bubblesort
+// First sorting algorithm Bubble sort
 function bubbleSort(arr) {
     var len = arr.length;
     for (var i = len - 1; i >= 0; i--) {
@@ -28,7 +28,7 @@ function bubbleSort(arr) {
     return arr;
 }
 
-// Second is Insertion Sort
+// Second is Insertion sort
 function insertionSort(arr) {
     var minIdx, temp,
         len = arr.length;
@@ -45,6 +45,58 @@ function insertionSort(arr) {
     }
     return arr;
 }
+
+// Third is Selection sort
+function selectionSort(arr){
+  var minIdx, temp, 
+      len = arr.length;
+  for(var i = 0; i < len; i++){
+    minIdx = i;
+    for(var  j = i+1; j<len; j++){
+       if(arr[j]<arr[minIdx]){
+          minIdx = j;
+       }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIdx];
+    arr[minIdx] = temp;
+  }
+  return arr;
+}
+
+// This is the third merge sort
+// Part 1
+function mergeSort(arr){
+   var len = arr.length;
+   if(len <2)
+      return arr;
+   var mid = Math.floor(len/2),
+       left = arr.slice(0,mid),
+       right =arr.slice(mid);
+   //send left and right to the mergeSort to broke it down into pieces
+   //then merge those
+   return merge(mergeSort(left),mergeSort(right));
+}
+// Part 2
+function merge(left, right){
+  var result = [],
+      lLen = left.length,
+      rLen = right.length,
+      l = 0,
+      r = 0;
+  while(l < lLen && r < rLen){
+     if(left[l] < right[r]){
+       result.push(left[l++]);
+     }
+     else{
+       result.push(right[r++]);
+    }
+  }  
+  //remaining part needs to be addred to the result
+  return result.concat(left.slice(l)).concat(right.slice(r));
+}
+   
+        
 
 document.getElementById('bubbleBtn').onclick = function handleClickEvent(ev) {
     arr = [];
@@ -69,7 +121,8 @@ document.getElementById('insertionBtn').onclick = function handleClickEvent(ev) 
     insertionSort(arr);
     document.getElementById('newArr').innerHTML = ('<h2> Here is your Insertion sorted array! ' + arr + '</h2>');
     document.getElementById('img').innerHTML="<img src='images/Insertion-sort-example.gif' />";
-    document.getElementById('output').innerHTML = ('<b>How Insertion sort works... </b></br> Imagine you are playing cards. Somebody is giving you cards one by one.' 
+    document.getElementById('output').innerHTML = ('<b>How Insertion sort works... </b></br>' 
+    + ' Imagine you are playing cards. Somebody is giving you cards one by one.' 
     + ' When you are receiving card, you are planning to put them in a way so that the smaller one is on the left. This means you want to insert them in a sorted way.' 
     + ' If the first card you are getting is 5. Just hold the card in your hand. you don’t have to do anything. If the second card is 2, you want to put it before 5 so that the two cards you have are sorted.' 
     + ' When you are putting the card with number 2 at the left, you are changing the position of the card 5 from first position to second position.' 
@@ -80,11 +133,19 @@ document.getElementById('insertionBtn').onclick = function handleClickEvent(ev) 
 
 }
 
-// document.getElementById('selectionBtn').onclick = function handleClickEvent(ev) {
-//     createArr(ev);
-//     bubbleSort(arr);
-//     document.getElementById('output').innerHTML = ('Here is your new array! ' + arr);
-// }
+document.getElementById('selectionBtn').onclick = function handleClickEvent(ev) {
+    arr = [];
+    createArr(ev);
+    selectionSort(arr);
+    document.getElementById('newArr').innerHTML = ('<h2> Here is your Selection sorted array! ' + arr + '</h2>');
+    document.getElementById('img').innerHTML="<img src='images/Selection-Gif.gif' />";
+    document.getElementById('output').innerHTML = ('<b>How Selection sort works... </b></br>' 
+    + ' This is very simple. Go through the array, find the index of the lowest element swap the lowest element with the first element.' 
+    + ' Hence first element is the lowest element in the array.' 
+    + ' Now go through the rest of the array (excluding the first element) and find the index of the lowest and swap it with the second element.' 
+    + ' Thats how it continues to select (find out) the lowest element of the array and putting it on the left until it hits the last element.');
+}
+
 
 // document.getElementById('mergerBtn').onclick = function handleClickEvent(ev) {
 //     createArr(ev);
